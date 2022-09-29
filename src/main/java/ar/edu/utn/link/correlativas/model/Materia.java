a38f1e0e14a262d02d607709a7ae4dfc1036bc9e
FeliMattei <felipemattei07@gmail.com>
2022-09-29 09:58:39 -0300

    Agregados los getters y setters de ID

--- src/main/java/ar/edu/utn/link/correlativas/model/Materia.java
+++ src/main/java/ar/edu/utn/link/correlativas/model/Materia.java
@@ -28,6 +28,14 @@ public class Materia {
 		this.nombre = nombre;
 	}
 
+	public Long getId(){
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public Integer getAnio() {
 		return anio;
 	}
